e8c2010d1ec28167951beea3c9b4fc9c11aca5f8
Grras Solutions Pvt Ltd <155415832+grrasolutions@users.noreply.github.com>
2024-01-08 09:35:23 +0530

    Day6.js

--- Day6.js
+++ Day6.js
@@ -1,12 +1,7 @@
-import turtle
-pen = turtle.Pen()
-pen.color('orange','green')
-pen.speed(0)
-pen.begin_fill()
-for sq in range(72):
-    for var in range(4):
-        pen.forward(100)
-        pen.left(90)
-    pen.left(5)
-pen.end_fill()
-turtle.exitonclick()
+function sumArray(arr) {
+    let sum = 0;  
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
